8cc8002202d7b3612a2e2782dacfb688baa1a7b6
s30213 <konradplaska@gmail.com>
2025-05-21 15:05:22 +0200

    First commit

---
+++ apbd19-cw12/Program.cs
@@ -0,0 +1,24 @@
+namespace apbd19_cw12;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
